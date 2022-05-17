e4e9d0c4d010b331b3bed6e8f6ac7dc07f033c2c
karthik polepeddy <ipkarthik@users.noreply.github.com>
2022-05-18 00:20:02 +0530

    Update Detail.js

--- src/Detail.js
+++ src/Detail.js
@@ -1,12 +1,27 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Navigate} from "react-router-dom";
 
 
 export default function Detail(props){ 
     let {id} = useParams();
+    let [story, setStory]=useState({});
 
     useEffect(()=>{
-        console.log(`${id}`);
+        const url="http://hn.algolia.com/api/v1/items/"+id;
+
+        const fetchData=async()=>{
+            try{
+                const response=await fetch(url);
+                const json=await response.json();
+                console.log(json);
+                setStory(json);
+            }
+            catch(error){
+                console.log("error", error);
+            }
+        };
+
+        fetchData();
     }, []);
 
     return(
@@ -14,6 +29,24 @@ export default function Detail(props){
             <div className="row">
                 <div className="col">
                     <h1>HackerNews</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p>{story.points} points</p>
+                </div>
+                <div className="col">
+                    <a href={story.url}>{story.title}</a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <a href="/">back</a>
                 </div>
             </div>
         </div>
