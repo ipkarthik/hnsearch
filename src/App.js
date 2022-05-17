993dd42ac7abef9f245b3cf15e3e64509df20662
karthik polepeddy <ipkarthik@users.noreply.github.com>
2022-05-17 16:47:52 +0530

    add query input

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="container">
+      <div className="row">
+        <div className="column">
+          <input className="form-control form-control-lg" type="text" placeholder="Enter search query" aria-label="search query"/>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column">
+          
+        </div>
+      </div>
     </div>
   );
 }
